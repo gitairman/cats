e4d0802f96b64f29bd120852d279852c382070a0
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-03-12 22:16:16 -0700

    implement async function tests

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -7,15 +7,18 @@ const breedDetailsFromFile = function (breed, callback) {
     console.log("In readFile's Callback: it has the data.");
     // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
     if (!error) callback(data);
+    if (error) callback(undefined);
   });
   // ISSUE: Attempting to return data out here will also not work.
   //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
 };
 
-// we try to get the return value
-breedDetailsFromFile('Bombay', (data) => {
-  console.log('Return Value: ', data);
-});
-breedDetailsFromFile('Balinese', (data) => {
-  console.log('Return Value: ', data);
-});
+// // we try to get the return value
+// breedDetailsFromFile('Bombay', (data) => {
+//   console.log('Return Value: ', data);
+// });
+// breedDetailsFromFile('Balinese', (data) => {
+//   console.log('Return Value: ', data);
+// });
+
+module.exports = breedDetailsFromFile;
